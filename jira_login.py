ccca0269d97e9e6fcb3769e47d398aa732ee920b
Atsushi Hosaka <ahosaka@indeed.com>
2019-08-15 13:53:29 -0500

    creat jira_login class

--- jira_login.py
+++ jira_login.py
@@ -1,15 +1,33 @@
 from jira import JIRA
-from jira.client import JIRA
+from jira.exceptions import JIRAError
 
 # jira login
-jira_user = " "  # Jira username
-jira_pswd = " "  # jira password
-options = {'server': ''}  # jira server url
 
-try:
-    jira = JIRA(options=options, basic_auth=(jira_user, jira_pswd))
-except JIRAError as e:
-    if e.status_code == 401:
-        print ("Login to JIRA failed.")
 
-print("Login!!")
+class JiraLogin:
+    def __init__(self, server, user, pas):
+        self.server = server
+        self.user = user
+        self.pas = pas
+
+    def login(self):
+
+        jira_user = self.user  # Jira username
+        jira_pswd = self.pas  # jira password
+        options = {'server': self.server}  # jira server url
+
+        try:
+            jira = JIRA(options=options, basic_auth=(jira_user, jira_pswd))
+
+        except JIRAError as e:
+            if e.status_code == 401:
+                print ("Login to JIRA failed.")
+
+        print("Login!!")
+
+        return
+
+
+# jira_login = JiraLogin("server", "username", "password")
+
+# jira_login.login()
